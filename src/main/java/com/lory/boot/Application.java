58a13d76b9ca6d153606297fe0eaf858b40f3f37
Lory Hou <lory.hou@augmentum.com>
2017-09-14 09:36:19 +0800

    refine code, remove useless code

--- src/main/java/com/lory/boot/Application.java
+++ src/main/java/com/lory/boot/Application.java
@@ -28,14 +28,7 @@ public class Application {
         try {
             double[][] gps = {{114.21892734521,29.575429778924},{114.21892734521,29.575429778924}};
             String response1 = GpsUtil.convertWGS84ToBD09(gps);
-
             logger.debug(response1);
-
-            String response = HttpUtil.get("http://api.map.baidu.com/geoconv/v1/?coords=114.21892734521,29.575429778924;114.21892734521,29.575429778924&from=1&to=5&ak=3053f47ef937c0205f11c05a65dda83e");
-
-            logger.debug(response);
-
-
         } catch (IOException e) {
             logger.error("cannot get baidu api",e);
 
